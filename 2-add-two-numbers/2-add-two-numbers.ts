16efc67c6607da8f4e10566215440f8f162c9fa6
Brian <brianbaso@gmail.com>
2022-08-02 11:51:27 -0400

    Time: 300 ms (6.79%), Space: 52.7 MB (5.15%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.ts
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let stack1 = [], stack2 = []
+    
+    while (l1 !== null) {
+        stack1.push(l1.val)
+        l1 = l1.next
+    }
+    
+    while (l2 !== null) {
+        stack2.push(l2.val)
+        l2 = l2.next
+    }
+    
+    let reverse1 = [], reverse2 = []
+    
+    while (stack1.length !== 0) {
+        let num = stack1.pop()
+        reverse1.push(num)
+    }
+    
+    while (stack2.length !== 0) {
+        let num = stack2.pop()
+        reverse2.push(num)
+    }
+    
+    const num1 = BigInt(reverse1.join(''))
+    const num2 = BigInt(reverse2.join(''))
+    console.log('num1', num1)
+    console.log('num2', num2)
+    const sum = num1 + num2
+    console.log('sum', sum)
+    
+    const sumArray = sum.toString().split('').map(Number)
+    
+    let prev = new ListNode(sumArray[0]);
+    let len = sumArray.length
+    
+    for (let i = 1; i < len; i++) {
+        const current = new ListNode(sumArray[i], prev);
+        prev = current
+    }
+    
+    return prev
+    
+};
