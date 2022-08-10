de007def964c0040f48ef23e13c1b96a77d4efc6
Brian <brianbaso@gmail.com>
2022-08-10 12:00:10 -0400

    Time: 127 ms (20.15%), Space: 44.9 MB (33.07%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.ts
@@ -0,0 +1,12 @@
+function longestCommonPrefix(strs: string[]): string {
+    
+    if (strs.length == 1) return strs[0]
+    
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[0][i] !== strs[j][i]) return strs[0].substring(0, i)
+        }
+    }
+    
+    return strs[0]
+};
